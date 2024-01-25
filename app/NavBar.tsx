c7c5ea4a33b9a465c9349169796f111dd80f525a
MAHossain1 <mahpro110@gmail.com>
2024-01-25 14:52:55 +0600

    navbar and active route styling done

---
+++ app/NavBar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+    { label: "About Us", href: "/about-us" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b px-5 h-14 mb-5 items-center text-2xl">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map(link => (
+          <Link
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            key={link.href}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;

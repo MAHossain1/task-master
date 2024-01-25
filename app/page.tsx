cabce8f40f95babb8f2aa3534f168c6812a9e6ed
MAHossain1 <mahpro110@gmail.com>
2024-01-25 16:13:21 +0600

    Create the issue model

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl text-center">Hello World</h1>
     </>
   );
 }

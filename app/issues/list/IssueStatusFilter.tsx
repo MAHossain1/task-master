d38f2f62fbdd2b9d36db8a481341b1aad4d176a8
MAHossain1 <mahpro110@gmail.com>
2024-03-03 15:41:49 +0600

    Filter issues

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,6 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value?: Status }[] = [
@@ -12,12 +13,19 @@ const statuses: { label: string; value?: Status }[] = [
 ];
 
 const IssueStatusFilter = () => {
+  const router = useRouter();
+
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={status => {
+        const query = status ? `?status=${status}` : "";
+        router.push("/issues/list" + query);
+      }}
+    >
       <Select.Trigger placeholder="Filter by status" />
       <Select.Content>
         {statuses.map(status => (
-          <Select.Item key={status.value} value={status.value || "1"}>
+          <Select.Item key={status.value} value={status.value || "0"}>
             {status.label}
           </Select.Item>
         ))}

53b7c5f2ef6c79415766bf809f9c45495d6cf169
MAHossain1 <mahpro110@gmail.com>
2024-03-05 17:32:49 +0600

    Update end point to update many

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,9 +1,16 @@
-import { z } from "zod";
+import { Status } from "@prisma/client";
+
+import { object, z } from "zod";
+
+const StatusEnum = z.enum(["OPEN", "IN_PROGRESS", "CLOSED"]);
 
 export const issueSchema = z.object({
   title: z.string().min(1, "Title is required").max(255),
   description: z.string().min(1, "Description is required").max(65535),
+  status: StatusEnum.optional(),
 });
+
+export const issueArraySchema = z.array(issueSchema);
 
 export const patchIssueSchema = z.object({
   title: z.string().min(1, "Title is required").max(255).optional(),
